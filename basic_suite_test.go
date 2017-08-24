ebbafd941df41dc6fb591dfae58647c7ceb67dfe
Jakub Labath <jakub@labath.ca>
2017-08-24 05:50:25 +0000

    refactor unit tests
    drop gocheck just use plain testing

---
+++ basic_suite_test.go
@@ -0,0 +1,362 @@
+package dbi
+
+import (
+	"errors"
+	"math/big"
+	"testing"
+	"time"
+)
+
+type BasicSuite struct{}
+
+func (s *BasicSuite) Name() string {
+	return "BasicSuite"
+}
+
+func (s *BasicSuite) Test1Create(t *testing.T, db *H) {
+	cp := &Company{}
+	db.DropTable(cp)
+	err := db.CreateTable(cp)
+	if err != nil {
+		t.Fatalf("could not create Company table %s", err)
+	}
+	//insert a record
+	cp.ID = 1
+	cp.Name = "IBM"
+	cp.Ticker = "IBM"
+	dbclmn, err := db.Insert(cp)
+	if err != nil {
+		t.Fatalf("could not insert a record %s", err)
+	}
+	//get a record
+	c1 := Company{ID: dbclmn.Val.(int64)}
+	err = db.Get(&c1)
+	if err != nil {
+		t.Fatalf("could not get a record %s", err)
+	}
+	if c1.ID != cp.ID {
+		t.Fatalf("IDs do not equal")
+	}
+	if c1.Name != cp.Name {
+		t.Fatalf("Name does not equal")
+	}
+	if c1.Ticker != cp.Ticker {
+		t.Fatalf("Ticker does not equal")
+	}
+	//update
+	c1.Name = "International Business Machines"
+	err = db.Update(&c1)
+
+	if err != nil {
+		t.Fatalf("could not update a record %s", err)
+	}
+
+	c2 := Company{ID: c1.ID}
+	err = db.Get(&c2)
+	if err != nil {
+		t.Fatalf("could not get a record %s", err)
+	}
+	if c1.ID != c2.ID {
+		t.Fatalf("IDs do not equal")
+	}
+	if c1.Name != c2.Name {
+		t.Fatalf("Name does not equal")
+	}
+	if c1.Ticker != c2.Ticker {
+		t.Fatalf("Ticker does not equal")
+	}
+
+	//test DB
+	dbHandle, err := db.DB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if dbHandle == nil {
+		t.Fatal("DB() handle should be non nil")
+	}
+}
+
+func (s *BasicSuite) Test2InsertSelect(t *testing.T, db *H) {
+	cp := &Company{}
+	db.DropTable(cp)
+	err := db.CreateTable(cp)
+	if err != nil {
+		t.Fatalf("Unable to create table %s", err)
+	}
+	sample := [][]string{
+		{"Red Hat", "RHT"},
+		{"Intel", "INTC"},
+		{"Google", "GOOG"},
+		{"IBM", "IBM"},
+		{"Oracle Corporation", "ORCL"},
+	}
+	for _, v := range sample {
+		cp.Name = v[0]
+		cp.Ticker = v[1]
+		_, err = db.Insert(cp)
+		if err != nil {
+			t.Fatalf("Unable to insert %s", err)
+		}
+	}
+	var results []*Company
+	err = db.Select(&results, "WHERE Ticker != ? ORDER BY ID", "INTC")
+	if err != nil {
+		t.Fatalf("Unable to select %s", err)
+	}
+	for _, v := range results {
+		if v.Ticker == "INTC" {
+			t.Fatalf("Expected INTC must be missing from results")
+		}
+	}
+}
+
+func (s *BasicSuite) Test4AnnualReports(t *testing.T, db *H) {
+	cp := Company{ID: 1}
+	if err := db.Get(&cp); err != nil {
+		t.Fatal(err)
+	}
+	ar := &AnnualReport{
+		CompanyID: cp.ID,
+		Year:      2015,
+		Sales:     big.NewInt(100000000000)}
+	db.DropTable(ar)
+	err := db.CreateTable(ar)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pk, err := db.Insert(ar)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ar2 := &AnnualReport{ID: pk.Val.(int64)}
+	err = db.Get(ar2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//check that values match
+	if ar2.CompanyID != cp.ID {
+		t.Fatal("CompanyID mismatch")
+	}
+	if ar2.Year != 2015 {
+		t.Fatal("year mismatch")
+	}
+	if ar2.Sales.String() != ar.Sales.String() {
+		t.Fatal("sales string mismatch")
+	}
+	if ar2.NetIncome != nil {
+		t.Fatal("NetIncome should be nil")
+	}
+	net := big.NewInt(50000000000)
+	ar2.NetIncome = net
+	err = db.Update(ar2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ar3 := &AnnualReport{ID: ar2.ID}
+	err = db.Get(ar3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ar3.CompanyID != cp.ID {
+		t.Fatal("CompanyID mismatch")
+	}
+	if ar3.Year != 2015 {
+		t.Fatal("year mismatch")
+	}
+	if ar3.Sales.String() != ar.Sales.String() {
+		t.Fatal("sales string mismatch")
+	}
+	if ar3.NetIncome.String() != net.String() {
+		t.Fatal("NetIncome mismatch")
+	}
+
+	//now run query on the reports
+	var results []AnnualReport
+	err = db.Select(&results, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(results) != 1 {
+		t.Fatal("results should have 1 result")
+	}
+	ar4 := results[0]
+	if ar4.CompanyID != cp.ID {
+		t.Fatal("CompanyID mismatch")
+	}
+	if ar4.Year != 2015 {
+		t.Fatal("year mismatch")
+	}
+	if ar4.Sales.String() != ar.Sales.String() {
+		t.Fatal("sales string mismatch")
+	}
+	if ar4.NetIncome.String() != net.String() {
+		t.Fatal("NetIncome mismatch")
+	}
+}
+
+var ErrBusted = errors.New("busted")
+
+type BustedResult struct {
+}
+
+func (b BustedResult) LastInsertId() (int64, error) {
+	return 0, ErrBusted
+}
+
+func (b BustedResult) RowsAffected() (int64, error) {
+	return 0, ErrBusted
+}
+
+func (s *BasicSuite) Test5PersonDemo(t *testing.T, db *H) {
+	p := &Person{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+	db.DropTable(p)
+	err := db.CreateTable(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pk, err := db.Insert(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	up := &Person{ID: pk.Val.(int)}
+	err = db.Get(up)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if up.LastName != "Doe" {
+		t.Fatal("LastName mismatch")
+	}
+
+	up.LastName = "Moe"
+	err = db.Update(up)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var results []Person
+	err = db.Select(&results, "WHERE last = ? ORDER BY last", "Moe")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(results) != 1 {
+		t.Fatal("results should have 1 result")
+	}
+
+	//test driver that does not support sql.Result
+	p1 := &Person{
+		FirstName: "A.T.",
+		LastName:  "Tappman",
+	}
+	pk, err = db.Insert(p1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn, err := db.DB()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx, err := conn.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newpk, err := db.lastInsertPKID(tx, p1, BustedResult{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if newpk.Val.(int) != pk.Val.(int) {
+		t.Fatal("newpk pk mismatch")
+	}
+	if err := tx.Commit(); err != nil {
+		t.Fatal(err)
+	}
+	p1.ID = newpk.Val.(int)
+	err = db.Get(p1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = db.Delete(p1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p2 := Person{ID: p1.ID}
+	err = db.Get(&p2)
+	if err != ErrNotFound {
+		t.Fatalf("want %v got %v", ErrNotFound, err)
+	}
+	p3 := Person{ID: 243}
+	err = db.Get(&p3)
+
+	if err != ErrNotFound {
+		t.Fatalf("want %v got %v", ErrNotFound, err)
+	}
+
+	p3.FirstName = "Steve"
+	p3.LastName = "Blank"
+	err = db.Update(&p3)
+	if err != ErrNotFound {
+		t.Fatalf("want %v got %v", ErrNotFound, err)
+	}
+}
+
+func (s *BasicSuite) Test6PersonNewDemo(t *testing.T, db *H) {
+	p := &Person{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+	err := db.DropTable(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = db.CreateTable(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pk, err := db.Insert(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	up := &Person{ID: pk.Val.(int)}
+	err = db.Get(up)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if up.LastName != "Doe" {
+		t.Fatalf("want %s got %s", "Doe", up.LastName)
+	}
+	up.LastName = "Moe"
+	err = db.Update(up)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !up.TimeStamp.IsZero() {
+		t.Fatal("timestamp should be Zero")
+	}
+	var results []Person
+	newF := func() RowUnmarshaler {
+		return &Person{
+			TimeStamp: time.Now(),
+		}
+	}
+	err = db.SelectNew(&results, newF, "WHERE last = ? ORDER BY last", "Moe")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(results) != 1 {
+		t.Fatal("results should have 1 result")
+	}
+
+	if results[0].TimeStamp.IsZero() {
+		t.Fatal("timestamp should not be Zero")
+	}
+}
--- crud_test.go
+++ crud_test.go
@@ -2,268 +2,140 @@ package dbi
 
 import (
 	"database/sql"
-	"errors"
-	"math/big"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
-	"time"
 
+	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
-	. "gopkg.in/check.v1"
 )
 
+type setupFunc func() (*H, error)
+type tearDownFunc func(db *H) error
+
+//sqlite
+func sqliteSetup() (*H, error) {
+	conn, err := sql.Open("sqlite3", "basic_suite.db")
+	if err != nil {
+		return nil, err
+	}
+	return New(conn)
+}
+
+func sqliteTearDown(db *H) error {
+	conn, err := db.DB()
+	if err != nil {
+		return err
+	}
+	conn.Close()
+	os.Remove("basic_suite.db")
+	return nil
+}
+
+//postgres pq
+func pqSetup() (*H, error) {
+	strconn := os.ExpandEnv("user=$PGUSER host=$PGHOST dbname=$PGDATABASE sslmode=disable")
+	conn, err := sql.Open("postgres", strconn)
+	if err != nil {
+		return nil, err
+	}
+
+	//overwrite pkMeta from models_test.go
+	pkMeta = &ColOpt{"SERIAL PRIMARY KEY", NoInsert | PrimaryKey}
+	blobMeta = &ColOpt{Type: "bytea"}
+	return New(conn, Postgres)
+}
+
+func pqTearDown(db *H) error {
+	conn, err := db.DB()
+	if err != nil {
+		return err
+	}
+	conn.Close()
+	return nil
+}
+
+type TestSuite interface {
+	Name() string
+}
+
+func TestDBI(t *testing.T) {
+	var tests = []struct {
+		name     string
+		setup    setupFunc
+		tearDown tearDownFunc
+		suits    []TestSuite
+	}{
+		{"sqlite", sqliteSetup, sqliteTearDown, []TestSuite{&BasicSuite{}}},
+		{"pq[postgres]", pqSetup, pqTearDown, []TestSuite{&BasicSuite{}}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for _, suite := range test.suits {
+				t.Run(suite.Name(), func(t *testing.T) {
+					db, err := test.setup()
+					if err != nil {
+						t.Error(err)
+					}
+					//run suite
+					runSuite(t, db, suite)
+					err = test.tearDown(db)
+					if err != nil {
+						t.Error(err)
+					}
+				})
+			}
+		})
+	}
+
+}
+
+//wrap t.Log as io.Writer
+//that DBI can then use
 type cWriter struct {
-	c *C
+	t *testing.T
 }
 
 func (w cWriter) Write(data []byte) (int, error) {
-	w.c.Log(strings.TrimSpace(string(data)))
+	w.t.Log(strings.TrimSpace(string(data)))
 	return len(data), nil
 }
 
-//hookup gocheck
-func Test(t *testing.T) { TestingT(t) }
-
-type BasicSuite struct {
-	testDB string
-	conn   *sql.DB
+//allow DBI logger to log via t.Log
+func localizeLogger(db *H, t *testing.T) (*H, error) {
+	fn := Logger(&cWriter{t})
+	err := fn(db)
+	return db, err
 }
 
-var _ = Suite(&BasicSuite{"basic_suite.db", nil})
-
-func (s *BasicSuite) SetUpSuite(c *C) {
-	var err error
-	s.conn, err = sql.Open("sqlite3", s.testDB)
-	c.Assert(err, IsNil)
-}
-
-func (s *BasicSuite) TearDownSuite(c *C) {
-	s.conn.Close()
-	os.Remove(s.testDB)
-}
-
-func (s *BasicSuite) Test1Create(c *C) {
-	db, err := New(s.conn, Logger(cWriter{c}))
-	c.Assert(err, IsNil)
-	cp := &Company{}
-	db.DropTable(cp)
-	err = db.CreateTable(cp)
-	c.Assert(err, IsNil)
-	//insert a record
-	cp.ID = 1
-	cp.Name = "IBM"
-	cp.Ticker = "IBM"
-	dbclmn, err := db.Insert(cp)
-	c.Assert(err, IsNil)
-	//get a record
-	c1 := Company{ID: dbclmn.Val.(int64)}
-	err = db.Get(&c1)
-	c.Assert(err, IsNil)
-	c.Assert(c1.ID, Equals, cp.ID)
-	c.Assert(c1.Name, Equals, cp.Name)
-	c.Assert(c1.Ticker, Equals, cp.Ticker)
-	//update
-	c1.Name = "International Business Machines"
-	err = db.Update(&c1)
-	c.Assert(err, IsNil)
-	c2 := Company{ID: c1.ID}
-	err = db.Get(&c2)
-	c.Assert(err, IsNil)
-	c.Assert(c2.ID, Equals, c1.ID)
-	c.Assert(c2.Name, Equals, c1.Name)
-	c.Assert(c2.Ticker, Equals, c1.Ticker)
-	//test DB
-	dbHandle, err := db.DB()
-	c.Assert(err, IsNil)
-	c.Assert(dbHandle, Equals, s.conn)
-}
-
-func (s *BasicSuite) Test2InsertSelect(c *C) {
-	db, err := New(s.conn, Logger(cWriter{c}))
-	c.Assert(err, IsNil)
-	cp := &Company{}
-	db.DropTable(cp)
-	err = db.CreateTable(cp)
-	c.Assert(err, IsNil)
-	sample := [][]string{
-		{"Red Hat", "RHT"},
-		{"Intel", "INTC"},
-		{"Google", "GOOG"},
-		{"IBM", "IBM"},
-		{"Oracle Corporation", "ORCL"},
+//executes each test method os suite in its own subtest
+func runSuite(t *testing.T, db *H, suite interface{}) {
+	rv := reflect.ValueOf(suite)
+	//if not pointer quit
+	if rv.Kind() != reflect.Ptr {
+		return
 	}
-	for _, v := range sample {
-		cp.Name = v[0]
-		cp.Ticker = v[1]
-		_, err = db.Insert(cp)
-		c.Assert(err, IsNil)
-	}
-	var results []*Company
-	err = db.Select(&results, "WHERE Ticker != ? ORDER BY ID", "INTC")
-	c.Assert(err, IsNil)
-	for _, v := range results {
-		c.Assert(v.Ticker, Not(Equals), "INTC")
-	}
-}
 
-func (s *BasicSuite) Test3NewDBAndUtility(c *C) {
-	const foo = "foo.db"
-	conn, err := sql.Open("sqlite3", foo)
-	c.Assert(err, IsNil)
-	db, err := New(conn)
-	c.Assert(err, IsNil)
-	c.Assert(db.conn, Equals, conn)
-	cp := &Company{}
-	err = db.CreateTable(cp)
-	c.Assert(err, IsNil)
-	err = conn.Close()
-	c.Assert(err, IsNil)
-	err = os.Remove(foo)
-	c.Assert(err, IsNil)
-}
+	rt := rv.Type()
+	for i := 0; i < rt.NumMethod(); i++ {
+		m := rt.Method(i)
+		if strings.HasPrefix(m.Name, "Test") {
+			t.Run(m.Name, func(t *testing.T) {
 
-func (s *BasicSuite) Test4AnnualReports(c *C) {
-	db, err := New(s.conn, Logger(cWriter{c}))
-	c.Assert(err, IsNil)
-	cp := Company{ID: 1}
-	err = db.Get(&cp)
-	c.Assert(err, IsNil)
-	ar := &AnnualReport{
-		CompanyID: cp.ID,
-		Year:      2015,
-		Sales:     big.NewInt(100000000000)}
-	err = db.CreateTable(ar)
-	c.Assert(err, IsNil)
-	pk, err := db.Insert(ar)
-	c.Assert(err, IsNil)
-	c.Assert(pk, FitsTypeOf, Col{})
-	ar2 := &AnnualReport{ID: pk.Val.(int64)}
-	err = db.Get(ar2)
-	c.Assert(err, IsNil)
-	//check that values match
-	c.Assert(ar2.CompanyID, Equals, cp.ID)
-	c.Assert(ar2.Year, Equals, 2015)
-	c.Assert(ar2.Sales.String(), Equals, ar.Sales.String())
-	c.Assert(ar2.NetIncome, IsNil)
-	net := big.NewInt(50000000000)
-	ar2.NetIncome = net
-	err = db.Update(ar2)
-	c.Assert(err, IsNil)
-	ar3 := &AnnualReport{ID: ar2.ID}
-	err = db.Get(ar3)
-	c.Assert(err, IsNil)
-	c.Assert(ar3.CompanyID, Equals, cp.ID)
-	c.Assert(ar3.Year, Equals, 2015)
-	c.Assert(ar3.Sales.String(), Equals, ar.Sales.String())
-	c.Assert(ar3.NetIncome.String(), Equals, net.String())
-	//now run query on the reports
-	var results []AnnualReport
-	err = db.Select(&results, "")
-	c.Assert(err, IsNil)
-	c.Assert(results, HasLen, 1)
-	ar4 := results[0]
-	c.Assert(ar4.CompanyID, Equals, cp.ID)
-	c.Assert(ar4.Year, Equals, 2015)
-	c.Assert(ar4.Sales.String(), Equals, ar.Sales.String())
-	c.Assert(ar4.NetIncome.String(), Equals, net.String())
-}
+				db, err := localizeLogger(db, t)
+				if err != nil {
+					t.Fatal(err)
+				}
 
-var ErrBusted = errors.New("busted")
-
-type BustedResult struct {
-}
-
-func (b BustedResult) LastInsertId() (int64, error) {
-	return 0, ErrBusted
-}
-
-func (b BustedResult) RowsAffected() (int64, error) {
-	return 0, ErrBusted
-}
-
-func (s *BasicSuite) Test5PersonDemo(c *C) {
-	db, err := New(s.conn, Logger(cWriter{c}))
-	c.Assert(err, IsNil)
-	p := &Person{
-		FirstName: "John",
-		LastName:  "Doe",
-	}
-	err = db.CreateTable(p)
-	c.Assert(err, IsNil)
-	pk, err := db.Insert(p)
-	c.Assert(err, IsNil)
-	c.Assert(pk, FitsTypeOf, Col{})
-	up := &Person{ID: pk.Val.(int)}
-	err = db.Get(up)
-	c.Assert(err, IsNil)
-	c.Assert(up.LastName, Equals, "Doe")
-	up.LastName = "Moe"
-	err = db.Update(up)
-	c.Assert(err, IsNil)
-	var results []Person
-	err = db.Select(&results, "WHERE last = ? ORDER BY last", "Moe")
-	c.Assert(err, IsNil)
-	c.Assert(results, HasLen, 1)
-	//test driver that does not support sql.Result
-	p1 := &Person{
-		FirstName: "A.T.",
-		LastName:  "Tappman",
-	}
-	pk, err = db.Insert(p1)
-	c.Assert(err, IsNil)
-	tx, err := s.conn.Begin()
-	c.Assert(err, IsNil)
-	newpk, err := db.lastInsertPKID(tx, p1, BustedResult{})
-	c.Assert(err, IsNil)
-	c.Assert(newpk.Val.(int), Equals, pk.Val.(int))
-	c.Assert(tx.Commit(), IsNil)
-	p1.ID = newpk.Val.(int)
-	c.Assert(db.Get(p1), IsNil)
-	err = db.Delete(p1)
-	c.Assert(err, IsNil)
-	p2 := Person{ID: p1.ID}
-	err = db.Get(&p2)
-	c.Assert(err, Equals, ErrNotFound)
-	p3 := Person{ID: 243}
-	err = db.Get(&p3)
-	c.Assert(err, Equals, ErrNotFound)
-	p3.FirstName = "Steve"
-	p3.LastName = "Blank"
-	err = db.Update(&p3)
-	c.Assert(err, Equals, ErrNotFound)
-}
-
-func (s *BasicSuite) Test6PersonNewDemo(c *C) {
-	db, err := New(s.conn, Logger(cWriter{c}))
-	c.Assert(err, IsNil)
-	p := &Person{
-		FirstName: "John",
-		LastName:  "Doe",
-	}
-	err = db.DropTable(p)
-	c.Assert(err, IsNil)
-	err = db.CreateTable(p)
-	c.Assert(err, IsNil)
-	pk, err := db.Insert(p)
-	c.Assert(err, IsNil)
-	c.Assert(pk, FitsTypeOf, Col{})
-	up := &Person{ID: pk.Val.(int)}
-	err = db.Get(up)
-	c.Assert(err, IsNil)
-	c.Assert(up.LastName, Equals, "Doe")
-	up.LastName = "Moe"
-	err = db.Update(up)
-	c.Assert(err, IsNil)
-	c.Assert(up.TimeStamp.IsZero(), Equals, true)
-	var results []Person
-	newF := func() RowUnmarshaler {
-		return &Person{
-			TimeStamp: time.Now(),
+				var args = []reflect.Value{
+					rv,
+					reflect.ValueOf(t),
+					reflect.ValueOf(db),
+				}
+				m.Func.Call(args)
+			})
 		}
+
 	}
-	err = db.SelectNew(&results, newF, "WHERE last = ? ORDER BY last", "Moe")
-	c.Assert(err, IsNil)
-	c.Assert(results, HasLen, 1)
-	c.Assert(results[0].TimeStamp.IsZero(), Equals, false)
 }
